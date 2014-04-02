5ba2542f2e7caa71f5edf11aad2ac9615851456b
Matt Oquist <moquist@majen.net>
2014-04-02 15:24:07 -0400

    add :exclusions [org.clojure/tools.macro] for compojure

--- project.clj
+++ project.clj
@@ -7,10 +7,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
+                 [compojure "1.1.5" :exclusions [org.clojure/tools.macro]]
                  [ring "1.2.1"]
                  [clout "1.1.0"]
                  [com.taoensso/timbre "3.0.1"]]
+  :pedantic? :warn
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.nrepl "0.2.3"]
                                   [org.clojure/tools.namespace "0.2.4"]
