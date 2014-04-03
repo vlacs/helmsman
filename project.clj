b0e9949c2961c1243d8c65b9dcca5aa634228f8f
Matt Oquist <moquist@majen.net>
2014-04-02 21:14:04 -0400

    make dep tree cleaner

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5" :exclusions [org.clojure/tools.macro]]
+                 [compojure "1.1.5" :exclusions [org.clojure/tools.macro ring/ring-core]]
                  [ring "1.2.1"]
                  [clout "1.1.0"]
                  [com.taoensso/timbre "3.0.1"]]
