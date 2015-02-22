4e5f2b0e907b4c548df4035581560e858bfeee30
Jon Doane <jdoane@vlacs.org>
2015-02-22 10:48:20 -0500

    Updated URI testing with different kinds of tests.

--- test/helmsman/test/uri.clj
+++ test/helmsman/test/uri.clj
@@ -1,38 +1,70 @@
 (ns helmsman.test.uri
   (:require
     [helmsman.uri]
+    [ring.util.codec]
     [clojure.test :as t]
     [clojure.test.check :as tc]
     [clojure.test.check.generators :as gen]
     [clojure.test.check.properties :as prop]))
 
-(def path-vector-gen
-  (gen/vector gen/string-alphanumeric)) 
+(def path-element-gen
+  (gen/one-of
+    [gen/string-alphanumeric
+     gen/keyword]))
 
-(def clean-vector-gen
-  (gen/vector (gen/such-that not-empty gen/string-alphanumeric)))
+(def path-vector-gen
+  (gen/vector
+    path-element-gen))
 
 (def path-string-gen
   (gen/fmap helmsman.uri/assemble path-vector-gen))
 
-;;; Ensures that noramlization strips out empty path items.
-(def uri-path-flat-normalize-property
+(def normalization-size-property
+  (prop/for-all
+    [item path-vector-gen]
+    (let [normalized (helmsman.uri/normalize-path item)]
+      (>= (count (flatten item)) (count normalized)))))
+
+(def normalization-reverse-set-property
   (prop/for-all
     [item path-vector-gen]
     (let [normalized (helmsman.uri/normalize-path item)
-          stripped (vec (filter not-empty item))]
-      (= normalized stripped)))) 
- 
-;;; Ensures that assembly->parse->assembly is reversible.
-(def uri-assemble-parse-property
+          normalized-set (set normalized)
+          contained
+          (vec
+            (keep
+              (fn [i]
+                (when (contains? normalized-set i)
+                  i))
+              (flatten item)))]
+      (= normalized contained))))
+
+(def normalization-subset-property
   (prop/for-all
-    [item clean-vector-gen]
-    (let [p (helmsman.uri/assemble item)
-          s (helmsman.uri/normalize-path
-              (helmsman.uri/path p))]
-      (= item s))))
+    [item path-vector-gen]
+    (let [normalized (helmsman.uri/normalize-path item)
+          normalized-set (set normalized)
+          raw-set (set (flatten item))]
+      (clojure.set/subset? normalized-set raw-set))))
+
+(def normalized-assembly-commutitive-property
+  (prop/for-all
+    [item path-vector-gen]
+    (let [normalized (helmsman.uri/normalize-path item)
+          assembled (helmsman.uri/assemble normalized)
+          disassembled (helmsman.uri/path assembled)]
+      (= normalized disassembled))))
 
 (t/deftest uri-testing
-  (t/is (:result (tc/quick-check 250 uri-path-flat-normalize-property)))
-  (t/is (:result (tc/quick-check 250 uri-assemble-parse-property))))
+  (t/testing "Normalization"
+    (t/is (:result (tc/quick-check 100 normalization-size-property))
+          "Flatted input vector should be the same size or bigger than the output.")
+    (t/is (:result (tc/quick-check 100 normalization-reverse-set-property))
+          "The input vector containing only items belonging to the set derived
+          from the normalized input vector should match the normal vector.")
+    (t/is (:result (tc/quick-check 100 normalization-subset-property))
+          "The output vector as a set should be a subset of the input.")
+    (t/is
+      (:result
+        (tc/quick-check 100 normalized-assembly-commutitive-property)))))
 
